4505d7a6e865b42c13f10fffde9634fb9ae287c9
Guillermo Figueroa <gafigueroa@uc.cl>
2017-01-26 10:26:14 -0800

    Lab3 modified

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -8,10 +8,31 @@ $(document).ready(function() {
 /*
  * Function that is called when the document is ready.
  */
-function initializePage() {
+
+	function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$('#testjs').text("If you press me right now everyone is going to die");
+		$('#testjs').addClass("btn btn-danger btn-lg");
+		$(".jumbotron p").toggleClass("active");
+
 	});
+
+
+	function projectClick() {
+		var containingProject = $(this).closest(".project");
+		var description = $(containingProject).find(".project-description");
+
+		console.log(description.length);
+		if (description.length == 0){
+			containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
+		}
+		else{
+			description.fadeOut();
+		}
+	};
+
+	$('.project').click(projectClick);
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
